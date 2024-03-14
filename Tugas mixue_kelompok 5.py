aa0ff9549bef6ad79782218a8e69064f3aa63a14
Nurfatehah7145 <147596928+Nurfatehah7145@users.noreply.github.com>
2024-03-14 22:15:30 +0700

    Update Tugas mixue_kelompok 5.py

--- Tugas mixue_kelompok 5.py
+++ Tugas mixue_kelompok 5.py
@@ -1,15 +1,17 @@
-"""Nama Kelompok
+"""Nama Kelompok 5
 1. FAUZA AHMAD ZAIDAN 23091397144
 2. NUR FATEHAH 23091397160
 3. AKHMAD FAUZI 23091397162
 """
 
 # Tugas Struktur Data 1 - Mixue
+
 class Pesanan:
     def _init_(self, nama_menu, harga, next=None):
         self.nama_menu = nama_menu
         self.harga = harga
         self.next = next
+
 
 class DaftarPesanan:
     def _init_(self):
@@ -26,7 +28,8 @@ class DaftarPesanan:
         while (temp.next):
             temp = temp.next
 
-        temp.next = pesanan
+         temp.next = pesanan
+
     
     def print(self):
         if self.head is None:
